6770845b1a81fe4251c84279247e4b91bca294bc
rikkerorbak <rikke.roerbaek.olsen@legohouse.com>
2022-03-29 16:59:02 +0200

    finished sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+   let result = 0;
 
+   if (typeof (start) != 'number' || typeof (end) != 'number' || start < 0 || end < 0) {
+      result = 'ERROR';
+   } else {
+      if (start > end) {
+         for (let i = end; i <= start; i++) {
+            result += i;
+         }
+      } else {
+         for (let i = start; i <= end; i++) {
+            result += i;
+         }
+      }
+   }
+   return result;
 };
 
 // Do not edit below this line

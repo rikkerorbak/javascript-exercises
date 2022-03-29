6692167679db73e2d69a70f1b71dda0c5a6d92c0
rikkerorbak <rikke.roerbaek.olsen@legohouse.com>
2022-03-29 15:31:40 +0200

    first removeFromArray

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
 
+const repeatString = function (str, num) {
+   let result = '';
+
+   if (!(num < 0)) {
+      for (let i = 0; i < num; i++) {
+         result += str;
+      }
+   } else result = 'ERROR'
+
+   return result;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, toRemove) {
+   // let newArray;
+   for (var i = 0; i < array.length; i++) {
+      if (array[i] === toRemove) {
+         array.splice(i, 1);
+      }
+   }
+   return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;

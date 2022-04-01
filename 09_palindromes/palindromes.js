b8e8768598e0ee33975d946ce6effb039441f54d
rikkerorbak <rikke.rorbak@gmail.com>
2022-04-01 15:32:09 +0200

    palindrome done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+   const strF = str.replaceAll(/[.,!\s]/g, '').toLowerCase();
+   let strB = '';
+   for (let i = strF.length - 1; i >= 0; i--) {
+      strB += strF[i];
+   }
+   return (strF === strB);
 };
 
 // Do not edit below this line
